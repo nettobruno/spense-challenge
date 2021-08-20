83362087ac5cd5d751ed4d0e4cb3c13859545e7f
Bruno Netto <brunonettomac@outlook.com>
2021-08-19 22:19:12 -0300

    Renaming images to keep project default

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import listImage from '../../Assets/Checkmark.svg'
+import listImage from '../../Assets/checkmark.svg'
 
 export const Container = styled.div`
   background-color: #fff;
