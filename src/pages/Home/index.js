2ffb817c78628d937d9e9de260a06f799fb6e684
Bruno Netto <brunonettomac@outlook.com>
2021-08-19 22:15:23 -0300

    adding general site font, text block, and image block in header section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,18 +2,41 @@
 import MenuHamburguer from '../../components/MenuHamburguer';
 
 // Images
-
+import headerImage from '../../Assets/hero-image-desktop.png';
 
 // Styles
-import { Container } from './styles';
+import { Container, Header } from './styles';
 
 function Home() {
   return (
     <Container>
       <MenuHamburguer />
-      <div className="container">
-        <h1>HomePage</h1>
-      </div>
+
+      <Header>
+        <div className="container grid">
+          <div id="texts">
+            <h1>Share you unfiltered thoughts. Get paid.</h1>
+            <p>Spense is an open platform for individuals to share their unfiltered thoughts.</p>
+            <p>Discuss the topics you love, and gat paid for doing <i>just</i> that.</p>
+
+            <ul>
+              <li>Receive 99% off the earnings.</li>
+              <li>Get paid via Debit Card, ACH, or PayPal.</li>
+              <li>Withdraw your earnings anytime.</li>
+            </ul>
+
+            <div className="send-email">
+              <input type="email" name="email" id="email" placeholder="john@example.com" />
+              <button type="submit">Get Started</button>
+            </div>
+          </div>
+
+          <div id="image">
+            <img src={headerImage} alt="" />
+          </div>
+        </div>
+      </Header>
+
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,10 +1,95 @@
 import styled from 'styled-components';
+import listImage from '../../Assets/Checkmark.svg'
 
 export const Container = styled.div`
-  background-color: rgba(55, 66, 250, 0.6);
+  background-color: #fff;
+`;
+
+export const Header = styled.div`
+  background-color: #e5e4e2;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   height: 100vh;
   width: 100%;
 
-  padding-top: 100px;
+
+  div.grid {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  div#texts {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  div#texts h1 {
+    color: #292929;
+    font-size: 3em;
+    margin-bottom: 15px;
+    width: 70%;
+  }
+
+  div#texts p {
+    color: #5A5444;
+    font-size: 1.2em;
+
+    &:nth-child(3) {
+      margin-bottom: 20px;
+    }
+  }
+
+  div#texts ul {
+    list-style-position: inside;
+    list-style-image: url(${listImage});
+    margin-bottom: 20px;
+  }
+
+  div#texts ul li {
+    color: #5A5444;
+    font-size: 1.4em;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  div.send-email {
+    /* border: 1px solid blue; */
+  }
+
+  div.send-email input {
+    border-radius: 5px;
+    border: 2px solid #d3d3d3;
+    color: #d3d3d3;
+    font-size: 1.2em;
+    margin-right: 20px;
+    padding: 15px 20px;
+
+    ::placeholder {
+      color: #d3d3d3;
+    }
+  }
+
+  div.send-email button {
+    background-color: #292929;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 1.2em;
+    padding: 15px 20px;
+    cursor: pointer;
+  }
+
+  div#image  {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div#image img {
+    width: 70%;
+  }
 `;
